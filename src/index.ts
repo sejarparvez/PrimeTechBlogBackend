268afe0b00ea56fa8da98344d84785173724dfaa
sejar parvez <sejarparvez@yahoo.com>
2023-05-03 11:38:46 +0600

    origin

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+
 dotenv.config();
-
 const app = express();
-const Port = process.env.PORT;
+const port = process.env.PORT;
+const allowedOrigin = process.env.ORIGIN;
 
-app.use(cors());
+const corsOptions: cors.CorsOptions = {
+  origin: allowedOrigin,
+  credentials: true,
+};
 
+// Add a CORS middleware to the app
+app.use(cors(corsOptions));
+
+// Define your endpoints
 app.get("/hello", (req, res) => {
   res.status(200).json("Hey Sejar parvez");
 });
 
-app.listen(Port, () => {
-  console.log(`Server is listening on port ${Port}`);
+app.get("/bye", (req, res) => {
+  res.status(200).json("Goodbye");
+});
+
+app.listen(port, () => {
+  console.log(`⚡ Server is listening on port ${port}`);
 });
